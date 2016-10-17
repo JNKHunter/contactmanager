b26af1bed85ddb498b5b440bbe34e91f402824dd
John Hunter <john@pushagency.io>
2016-10-16 18:58:31 -0700

    refactored contacts data access

--- src/main/java/tech/eats/art/contactmanager/Application.java
+++ src/main/java/tech/eats/art/contactmanager/Application.java
@@ -1,5 +1,6 @@
 package tech.eats.art.contactmanager;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
@@ -7,6 +8,8 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.service.ServiceRegistry;
 import tech.eats.art.contactmanager.model.Contact;
+
+import java.util.List;
 
 /**
  * Created by John on 10/16/16.
@@ -26,6 +29,27 @@ public class Application {
                 .withPhone(1234567l)
                 .build();
 
+        save(contact);
+
+        for(Contact c : fetchAllContacts()){
+            System.out.println(c);
+        }
+
+    }
+
+    @SuppressWarnings("unchecked")
+    private static List<Contact> fetchAllContacts(){
+        Session session = sessionFactory.openSession();
+
+        Criteria criteria = session.createCriteria(Contact.class);
+        List<Contact> contacts = criteria.list();
+
+        session.close();
+
+        return contacts;
+    }
+
+    private static void save(Contact contact) {
         Session session = sessionFactory.openSession();
         session.beginTransaction();
         session.save(contact);
