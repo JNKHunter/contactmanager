f779713e9c96353b3425fb224b628665afa103dc
John Hunter <john@pushagency.io>
2016-10-16 18:34:48 -0700

    added sessionfactory and builder

--- src/main/java/tech/eats/art/contactmanager/Application.java
+++ src/main/java/tech/eats/art/contactmanager/Application.java
@@ -1,9 +1,22 @@
 package tech.eats.art.contactmanager;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
 /**
  * Created by John on 10/16/16.
  */
 public class Application {
+
+    public static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        final ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
 
     public static void main(String[] args) {
 
